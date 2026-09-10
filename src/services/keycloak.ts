085e3d3dd4508bda2d18756b815c3872f6136c3e
Rakesh Kumar <rakeshfreomsiraha@gmail.com>
2026-09-10 05:54:35 +0530

    Configure production Keycloak

--- src/services/keycloak.ts
+++ src/services/keycloak.ts
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-  url: 'http://localhost:8081',
+  url: 'https://keycloak-production-66a8.up.railway.app',
   realm: 'capacity-connect',
   clientId: 'capacity-connect-frontend',
 });
